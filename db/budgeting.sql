35d4d50ff70dfd6042b933bfa2703fe30e0152a9
staceynapier <staceynapier86@gmail.com>
2017-07-21 12:59:55 +0100

    Initial commit

---
+++ db/budgeting.sql
@@ -0,0 +1,16 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+
+CREATE TABLE tags (
+  id SERIAL4 PRIMARY KEY,
+  type VARCHAR(255),
+  icon VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+  id SERIAL4 PRIMARY KEY,
+  merchant VARCHAR(255),
+  price INT4,
+  date_of_trans DATE,
+  tag_id INT4 REFERENCES tags(id)
+);
